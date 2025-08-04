0783230fafdedb161b3ec49818084477ce2908e7
prerak-talc <prerak.talc@gmail.com>
2025-08-05 01:22:22 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdCtN1WsoNtMFAwEUDv34pJy4lT6a3I38",
-  authDomain: "kpi-talc.firebaseapp.com",
-  projectId: "kpi-talc",
-  storageBucket: "kpi-talc.firebasestorage.app",
-  messagingSenderId: "67217863373",
-  appId: "1:67217863373:web:d014a8576b8bd7cfdd6f84"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
